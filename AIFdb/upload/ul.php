db9f152b2f801ff3e6253fecb827b6853e7d415b
Ray Becker <raybecker@gmail.com>
2022-06-21 15:10:53 +0100

    Update ul.php
    Similar to json.php, I've made a $move_test variable to evaluate before and not in the if statement on ln12 and added ln11 with var_dump(get_defined_vars()) for more debugging formation than the echo on ln46

--- AIFdb/upload/ul.php
+++ AIFdb/upload/ul.php
@@ -1,13 +1,15 @@
 <?php
 
-$host = 'www.aifdb.org';
+$host = 'localhost';
 $db = '/';
 
 $target_path = "tmp/";
 $fname = basename($_FILES['uploadedfile']['name']);
 $target_path = $target_path . $fname; 
-
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+# echo $target_path;
+$move_test = move_uploaded_file($_FILES['file']['tmp_name'], $target_path);
+var_dump(get_defined_vars());
+if($move_test != FALSE) {
     if($_POST['ftype'] == "aml"){
         require_once('amlparse/AmlAif.php');
         $a = new AmlAif(file_get_contents($target_path),$host,$db,'test','pass');
@@ -20,17 +22,24 @@ if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible;)");
         curl_setopt($ch, CURLOPT_URL, "http://$host$db".$_POST['ftype']."/");
-        curl_setopt($ch, CURLOPT_USERPWD,"test:pass"); 
+        curl_setopt($ch, CURLOPT_USERPWD,"root:@rgtech"); 
         curl_setopt($ch, CURLOPT_POST, true);
         $post = array(
             "file"=>"@".$target_path,
         );
+
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post); 
-        //$response = curl_exec($ch);
-        if( ! $result = curl_exec($ch)) {
+		
+		//for debug only!
+		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        
+		if( ! $result = curl_exec($ch)) {
             trigger_error(curl_error($ch)); 
         } 
         curl_close($ch);
+		var_dump($response);
         echo $result;
     }
 }else{
